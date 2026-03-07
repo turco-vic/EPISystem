d41ef4c33a072f39d6179dd9c555491374e3710f
Enzo Turcovic <enzoturcovic@gmail.com>
2026-03-06 21:35:05 -0300

    Add Vercel Analytics integration

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import { inject } from "@vercel/analytics"
 
 const app = createApp(App)
 
 app.use(router)
 app.mount('#app')
+
+inject()
