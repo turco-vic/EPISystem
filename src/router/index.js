fa10fa13587e0d6761c90d8a1782cb832aca4b4d
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2026-06-08 14:33:10 -0300

    Reset scroll to top on route change

--- src/router/index.js
+++ src/router/index.js
@@ -52,6 +52,9 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior() {
+        return { top: 0 }
+    }
 })
 
 router.beforeEach(async (to) => {
